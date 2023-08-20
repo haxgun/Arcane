1e19d2ffa7deecb3c835541e2a036227a187a4ac
Misha Gusev <majikx.prod0@gmail.com>
2023-08-20 13:59:27 +0500

    feat(main): command-line arguments are placed in a separate file and a description of the arguments is added

---
+++ haxbod/utils/command_line_arguments.py
@@ -0,0 +1,42 @@
+from pony.orm import db_session
+
+from haxbod.bot import Haxbod
+from haxbod.models import db
+from haxbod.utils.twitchapi import existing_channel_twitch
+from haxbod.utils.print import print_success, print_error, input_answer
+
+bot = Haxbod()
+
+
+def add_channel():
+    channel_name = str(input_answer('Which channel do you want to add?'))
+
+    if not existing_channel_twitch(channel_name):
+        print_error('There is no such user!')
+        return
+
+    with db_session:
+        existing_channel_db = db.Channel.get(name=channel_name)
+
+        if not existing_channel_db:
+            channel = db.Channel(name=channel_name)
+            print_success(f'User @{channel_name} added.')
+        else:
+            print_error(f'User @{channel_name} already exists.')
+
+
+def run_bot():
+    bot.run()
+
+
+def remove_channel():
+    channel_name = str(input_answer('Which channel do you want to add?'))
+
+    with db_session:
+        existing_channel = db.Channel.get(name=channel_name)
+
+        if existing_channel:
+            existing_channel.delete()
+            print_success(f'User @{channel_name} has been removed from the database.')
+        else:
+            print_error(f'User @{channel_name} is not in the database.')
--- main.py
+++ main.py
@@ -1,65 +1,41 @@
 # !/usr/bin/env python
+"""Haxbod command-line utility for administrative tasks."""
 
 import argparse
 
-from pony.orm import db_session
-
-from haxbod.bot import Haxbod
-from haxbod.models import db
-from haxbod.utils.twitchapi import existing_channel_twitch
-from haxbod.utils.print import print_success, print_error, input_answer
-
-bot = Haxbod()
-
-
-def add_channel():
-    channel_name = str(input_answer('Which channel do you want to add?'))
-
-    if not existing_channel_twitch(channel_name):
-        print_error('There is no such user!')
-        return
-
-    with db_session:
-        existing_channel_db = db.Channel.get(name=channel_name)
-
-        if not existing_channel_db:
-            channel = db.Channel(name=channel_name)
-            print_success(f'User @{channel_name} added.')
-        else:
-            print_error(f'User @{channel_name} already exists.')
-
-
-def run_bot():
-    bot.run()
-
-
-def remove_channel():
-    channel_name = str(input_answer('Which channel do you want to add?'))
-
-    with db_session:
-        existing_channel = db.Channel.get(name=channel_name)
-
-        if existing_channel:
-            existing_channel.delete()
-            print_success(f'User @{channel_name} has been removed from the database.')
-        else:
-            print_error(f'User @{channel_name} is not in the database.')
+from haxbod.utils.print import print_error
+from haxbod.utils.command_line_arguments import add_channel, run_bot, remove_channel
 
 
 def main():
+    """Run administrative tasks."""
     command_functions = {
         'addchannel': add_channel,
         'runbot': run_bot,
         'removechannel': remove_channel
     }
 
+    command_descriptions = {
+        'addchannel': 'Add a channel to the bot',
+        'runbot': 'Run the bot',
+        'removechannel': 'Remove a channel from the bot'
+    }
+
     parser = argparse.ArgumentParser(description='Script to interact with your Twitch bot')
-    parser.add_argument('command', choices=command_functions.keys(), help='Specify the command to execute')
+
+    subparsers = parser.add_subparsers(dest='command', help='Specify the command to execute')
+
+    addchannel_parser = subparsers.add_parser('addchannel', help=command_descriptions['addchannel'])
+    runbot_parser = subparsers.add_parser('runbot', help=command_descriptions['runbot'])
+    removechannel_parser = subparsers.add_parser('removechannel', help=command_descriptions['removechannel'])
 
     args = parser.parse_args()
 
-    selected_function = command_functions[args.command]
-    selected_function()
+    try:
+        selected_function = command_functions[args.command]
+        selected_function()
+    except KeyError:
+        print_error('No argument selected')
 
 
 if __name__ == '__main__':
