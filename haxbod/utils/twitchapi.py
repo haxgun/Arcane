5244065a3eb9dad5e54b910438c2d822f4c75840
Misha Gusev <majikx.prod0@gmail.com>
2023-08-20 05:10:23 +0500

    feat: add twitchapi

---
+++ haxbod/utils/twitchapi.py
@@ -0,0 +1,22 @@
+import requests
+from haxbod import settings
+
+
+def channel_exists(channel_name):
+    client_id = settings.CLIENT_ID
+    access_token = settings.ACCESS_TOKEN
+
+    headers = {
+        'Client-ID': client_id,
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    url = f'https://api.twitch.tv/helix/users?login={channel_name}'
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    if response.status_code == 200 and data.get('data'):
+        return True
+    else:
+        return False
