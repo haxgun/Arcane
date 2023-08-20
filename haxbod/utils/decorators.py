233394bc1c410a7abd2e4794481fe00cac900cae
Misha Gusev <majikx.prod0@gmail.com>
2023-08-20 21:33:54 +0500

    feat(decorators): add permission decorator

--- haxbod/utils/decorators.py
+++ haxbod/utils/decorators.py
@@ -4,9 +4,27 @@ from haxbod import settings
 def owner_only():
     def decorator(func):
         async def wrapper(self, ctx, *args, **kwargs):
-            if str(ctx.author.id) == settings.OWNER_ID:
+            if ctx.author.id == settings.OWNER_ID:
                 await func(self, ctx, *args, **kwargs)
-            else:
-                await ctx.reply(f'Sorry, {ctx.author.name}, you do not have permission to use this command.')
+        return wrapper
+    return decorator
+
+
+def check_user_roles(ctx):
+    user_roles = []
+    if ctx.author.is_broadcaster: user_roles.append('broadcaster')
+    if ctx.author.is_mod: user_roles.append('moderator')
+    if ctx.author.is_subscriber: user_roles.append('subscriber')
+    if ctx.author.is_turbo: user_roles.append('turbo')
+    if ctx.author.is_vip: user_roles.append('vip')
+    return user_roles
+
+
+def permission(*allowed_roles):
+    def decorator(func):
+        async def wrapper(self, ctx, *args, **kwargs):
+            for role in allowed_roles:
+                if role in check_user_roles(ctx):
+                    return await func(self, ctx, *args, **kwargs)
         return wrapper
     return decorator
