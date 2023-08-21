4ee6245f0a87212108329abd1f50b0e646c89040
Misha Gusev <majikx.prod0@gmail.com>
2023-08-21 21:23:43 +0500

    feat(moderator): add custom command delete and edit command

--- haxbod/cogs/moderator.py
+++ haxbod/cogs/moderator.py
@@ -12,27 +12,68 @@ class Moderator(commands.Cog):
 
     @commands.command(name='addcommand', aliases=['addcmd'])
     @permission('moderator', 'broadcaster')
+    @db_session
     async def cmd_add_command(self, ctx: commands.Context, *args) -> None:
         if len(args) >= 2:
             command_name = args[0]
             response = ' '.join(args[1:])
 
-            with db_session:
-                channel = db.Channel.get(name=ctx.channel.name)
+            channel = db.Channel.get(name=ctx.channel.name)
+            command = db.Command.get(name=command_name, channel=channel.id)
 
+            if not command:
                 command = db.Command(
                     name=command_name,
                     response=response,
                     channel=channel.id,
                 )
-
-            if command:
-                await ctx.reply(f'Command !{command_name} has been added.')
             else:
-                await ctx.reply(f'Command !{command_name} already exist.')
+                await ctx.reply(f'Command !{command_name} already exists.')
+                return
+            await ctx.reply(f'Command !{command_name} has been added.')
         else:
-            command = ctx.message.content.split()[0]
-            await ctx.reply(f'Usage: {command} <command> <response>')
+            main_command = ctx.message.content.split()[0]
+            await ctx.reply(f'Usage: {main_command} <command> <response>')
+
+    @commands.command(name='editcommand', aliases=['editcmd'])
+    @permission('moderator', 'broadcaster')
+    @db_session
+    async def cmd_edit_command(self, ctx: commands.Context, *args) -> None:
+        if len(args) >= 2:
+            command_name = args[0]
+            new_response = ' '.join(args[1:])
+
+            channel = db.Channel.get(name=ctx.channel.name)
+            command = db.Command.get(name=command_name, channel=channel.id)
+
+            if channel and command:
+                command.response = new_response
+                db.commit()
+                await ctx.reply(f'Command !{command_name} has been updated.')
+            else:
+                await ctx.reply(f'Command !{command_name} not found.')
+        else:
+            main_command = ctx.message.content.split()[0]
+            await ctx.reply(f'Usage: {main_command} <command> <response>')
+
+    @commands.command(name='delcommand', aliases=['delcmd'])
+    @permission('moderator', 'broadcaster')
+    @db_session
+    async def cmd_delete_command(self, ctx: commands.Context, *args) -> None:
+        if len(args) >= 1:
+            command_name = args[0]
+
+            channel = db.Channel.get(name=ctx.channel.name)
+            command = db.Command.get(name=command_name, channel=channel.id)
+
+            if channel and command:
+                command.delete()
+                await ctx.reply(f'Command !{command_name} has been deleted.')
+            else:
+                await ctx.reply(f'Command !{command_name} not found.')
+        else:
+            main_command = ctx.message.content.split()[0]
+            await ctx.reply(f'Usage: {main_command} <command> <response>')
 
 
 def prepare(bot: Haxbod) -> None:
