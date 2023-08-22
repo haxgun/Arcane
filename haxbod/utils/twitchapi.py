c930e0ee86699233eecea787a174dc490e985fc5
Misha Gusev <majikx.prod0@gmail.com>
2023-08-22 16:04:13 +0500

    refactor

--- haxbod/utils/twitchapi.py
+++ haxbod/utils/twitchapi.py
@@ -5,18 +5,12 @@ from haxbod import settings
 def existing_channel_twitch(channel_name: str) -> bool:
     client_id = settings.CLIENT_ID
     access_token = settings.ACCESS_TOKEN
-
     headers = {
         'Client-ID': client_id,
         'Authorization': f'Bearer {access_token}'
     }
-
     url = f'https://api.twitch.tv/helix/users?login={channel_name}'
-
     response = requests.get(url, headers=headers)
-    data = response.json()
-
-    if response.status_code == 200 and data.get('data'):
+    if response.ok and response.json().get('data'):
         return True
-    else:
-        return False
+    return False
