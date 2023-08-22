53192096a435af0b7e1ef05b90abead3e0b01517
Misha Gusev <majikx.prod0@gmail.com>
2023-08-22 15:42:59 +0500

    perf

--- haxbod/settings/settings.py
+++ haxbod/settings/settings.py
@@ -1,18 +1,21 @@
-import os
-import dotenv
+from os import environ
+from dotenv import load_dotenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 dotenv_file = BASE_DIR / '.env'
 if dotenv_file.is_file():
-    dotenv.load_dotenv(dotenv_file)
+    load_dotenv(dotenv_file)
 
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-REFRESH_TOKEN = os.environ['REFRESH_TOKEN']
-CLIENT_ID = os.environ['CLIENT_ID']
+# Twitch Bot Token
+ACCESS_TOKEN = environ['ACCESS_TOKEN']
+REFRESH_TOKEN = environ['REFRESH_TOKEN']
+CLIENT_ID = environ['CLIENT_ID']
 
-PREFIX = os.environ['PREFIX']
-OWNER_ID = os.environ['OWNER_ID']
+# Bot settings
+PREFIX = environ['PREFIX']
+OWNER_ID = environ['OWNER_ID']
 
-DB_NAME = os.environ['DB_NAME']
+# DB settings
+DB_NAME = environ['DB_NAME']
