f11b1594671935db4560b18fcd0e4665eaa54176
starlitskies <majikx.prod0@gmail.com>
2023-09-10 12:03:56 +0500

    feat(settings): remove IRC and OAUTH_TOKEN

--- arcane/settings/settings.py
+++ arcane/settings/settings.py
@@ -9,13 +9,8 @@ dotenv_file = BASE_DIR / '.env'
 if dotenv_file.is_file():
     load_dotenv(dotenv_file)
 
-# IRC
-irc_server = 'irc.chat.twitch.tv'
-irc_port = 6697
-
 # Twitch Bot Settings
 USERNAME = environ['USERNAME']
-OAUTH_TOKEN = environ['OAUTH_TOKEN']
 ACCESS_TOKEN = environ['ACCESS_TOKEN']
 REFRESH_TOKEN = environ['REFRESH_TOKEN']
 CLIENT_ID = environ['CLIENT_ID']
