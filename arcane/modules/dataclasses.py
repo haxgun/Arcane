dda55064d102d4b340c512f0c820de9a21e30c92
starlitskies <majikx.prod0@gmail.com>
2023-09-10 16:41:44 +0500

    feat(dataclasses): add parse in Message and change attribute

--- arcane/modules/dataclasses.py
+++ arcane/modules/dataclasses.py
@@ -1,12 +1,33 @@
-from typing import NamedTuple
+import re
+from typing import NamedTuple, Optional
+
+
+MSG_RE = re.compile(
+    '^@(?P<info>[^ ]+) :(?P<author>[^!]+).* '
+    'PRIVMSG #(?P<channel>[^ ]+) '
+    ':(?P<message>[^\r]+)',
+)
 
 
 class Message(NamedTuple):
-    prefix: str
+    info: dict[str, str]
     author: str
     channel: str
-    irc_command: str
-    irc_args: list
-    content: str
-    content_command: str
-    content_args: list
+    message: str
+
+    @classmethod
+    def parse(cls, msg: str) -> Optional['Message']:
+        match = MSG_RE.match(msg)
+        if match:
+            info = {}
+            for part in match['info'].split(';'):
+                k, v = part.split('=', 1)
+                info[k] = v
+
+            return cls(
+                info=info,
+                author=match['author'],
+                channel=match['channel'],
+                message=match['message']
+            )
+        return None
