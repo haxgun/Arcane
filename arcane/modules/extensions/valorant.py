0f1338e88e476f152f65b91879f40141509c7665
starlitskies <majikx.prod0@gmail.com>
2023-09-28 05:24:06 +0500

    feat(valorant): add a condition if there is no valorant name

--- arcane/modules/extensions/valorant.py
+++ arcane/modules/extensions/valorant.py
@@ -9,11 +9,12 @@ async def cmd_valorant_rank(msg: Message, valorant_name: str = None) -> None:
     if not valorant_name or '#' not in valorant_name:
         channel = Channel.get(Channel.name == msg.channel)
         valorant_name = channel.valorant
-    info = await get_rank_with_rr_and_elo(valorant_name)
-    if info:
-        await msg.reply(info)
-    else:
-        await msg.reply('ERROR')
+    if not valorant_name:
+        info = await get_rank_with_rr_and_elo(valorant_name)
+        if info:
+            await msg.reply(info)
+        else:
+            await msg.reply('ERROR')
 
 
 @bot.command(name='lastgame', aliases=['lg'])
@@ -21,8 +22,9 @@ async def cmd_valorant_lg(msg: Message, valorant_name: str = None) -> None:
     if not valorant_name or '#' not in valorant_name:
         channel = Channel.get(Channel.name == msg.channel)
         valorant_name = channel.valorant
-    info = await get_stats_last_game(valorant_name)
-    if info:
-        await msg.reply(info)
-    else:
-        await msg.reply('ERROR')
+    if not valorant_name:
+        info = await get_stats_last_game(valorant_name)
+        if info:
+            await msg.reply(info)
+        else:
+            await msg.reply('ERROR')
