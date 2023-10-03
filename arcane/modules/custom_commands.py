eff33682379a743172074840a66c2281fb8e4b3a
starlitskies <majikx.prod0@gmail.com>
2023-10-03 06:44:11 +0500

    feat(custom_commands): update to the latest version of the engine

--- arcane/modules/custom_commands.py
+++ arcane/modules/custom_commands.py
@@ -4,6 +4,7 @@ import emoji
 from peewee import DoesNotExist
 
 from arcane.models import Channel, Command, Alias
+from arcane.modules.dataclasses import Message
 
 cooldowns = {}
 
@@ -12,30 +13,30 @@ def starts_with_emoji(text) -> bool:
     return emoji.demojize(text) != text
 
 
-async def find_entity(ctx, EntityModel) -> bool:
-    entity_name = ctx.message.content.split()[0][1:]
+async def find_entity(msg: Message, EntityModel) -> bool:
+    entity_name = msg.content.split()[0][1:]
 
     if starts_with_emoji(entity_name):
         return False
 
     try:
-        channel = Channel.get(Channel.name == ctx.channel.name)
+        channel = Channel.get(Channel.name == msg.channel)
         entity = EntityModel.get(EntityModel.name == entity_name, EntityModel.channel == channel)
         return True
     except DoesNotExist:
         return False
 
 
-async def custom_command_response(ctx) -> str:
-    command_name = ctx.message.content.split()[0][1:]
-    channel = Channel.get(Channel.name == ctx.channel.name)
+async def custom_command_response(msg: Message) -> str:
+    command_name = msg.content.split()[0][1:]
+    channel = Channel.get(Channel.name == msg.channel)
     command = Command.get(Command.name == command_name, Command.channel == channel)
     return command.response
 
 
-async def alias_response(ctx) -> str:
-    alias_name = ctx.message.content.split()[0][1:]
-    channel = Channel.get(Channel.name == ctx.channel.name)
+async def alias_response(msg: Message) -> str:
+    alias_name = msg.content.split()[0][1:]
+    channel = Channel.get(Channel.name == msg.channel)
     alias = Alias.get(Alias.name == alias_name, Alias.channel == channel)
     command = alias.command
     return command.response
@@ -57,30 +58,30 @@ async def update_command_cooldown(user_id: str, command: str) -> None:
     cooldowns[key] = time.time()
 
 
-async def handle_custom_commands(context) -> None:
+async def handle_custom_commands(msg: Message) -> None:
     entity = None
     response = None
     count = 1
 
-    if context.author.is_broadcaster or context.author.is_mod:
-        if len(context.message.content.split()) > 1:
+    if msg.author.is_broadcaster or msg.author.is_moderator:
+        if len(msg.content.split()) > 1:
             try:
-                count = int(context.message.content.split()[1])
+                count = int(msg.content.split()[1])
             except ValueError:
                 pass
 
-    if await find_entity(context, Command):
+    if await find_entity(msg, Command):
         entity = Command
-        response = await custom_command_response(context)
-    elif await find_entity(context, Alias):
+        response = await custom_command_response(msg)
+    elif await find_entity(msg, Alias):
         entity = Alias
-        response = await alias_response(context)
+        response = await alias_response(msg)
 
     if entity and response:
-        user_id = context.message.author.id
-        command = context.message.content.split()[0][1:]
+        user_id = msg.author.id
+        command = msg.content.split()[0][1:]
         cooldown_duration = 5
         if await can_use_command(user_id, command, cooldown_duration):
             await update_command_cooldown(user_id, command)
             for _ in range(count):
-                await context.send(response) if count > 1 else await context.reply(response)
+                await msg.send(response) if count > 1 else await msg.reply(response)
